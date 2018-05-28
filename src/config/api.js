c869d2ec058208d8cf12ed755c175630e7ddd502
German <german.goldenstein@gmail.com>
2018-05-28 10:59:11 -0300

    Add firebase and config

---
+++ src/config/api.js
@@ -0,0 +1,7 @@
+const api = {
+  localhost: 'http://localhost:3001', // Replace this url for your local API server
+  production: 'https://ibs-webrtc-ventures-api.herokuapp.com', // Replace this url for your production API server
+};
+
+export default api;
+ 
---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+const firebase = {
+  apiKey: 'AIzaSyD258TCwPWmw8Nj1vFm0DKQlMV3hRTaC4s', // firebase apikey
+  authDomain: 'ibs-webrtc-ventures.firebaseapp.com', // firebase authDomain
+  databaseURL: 'https://ibs-webrtc-ventures.firebaseio.com', // firebase databaseURL
+  projectId: 'ibs-webrtc-ventures', // firebase projectId
+  storageBucket: 'ibs-webrtc-ventures.appspot.com', // firebase storageBucket
+  messagingSenderId: '178304080524',  // firebase messagingSenderId
+};
+
+export default firebase;
+ 
