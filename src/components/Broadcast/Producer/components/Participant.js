4ad2611919777f095dab809a9f41024842fe37df
deviantfero <fmorataya.04@gmail.com>
2018-11-08 16:53:16 -0600

    use the new translateRole function

--- src/components/Broadcast/Producer/components/Participant.js
+++ src/components/Broadcast/Producer/components/Participant.js
@@ -6,7 +6,7 @@ import classNames from 'classnames';
 import Icon from 'react-fontawesome';
 import CopyToClipboard from '../../../Common/CopyToClipboard';
 import createUrls from '../../../../services/eventUrls';
-import { isFan, getRoleTranslation } from '../../../../services/util';
+import { isFan, translateRole } from '../../../../services/util';
 import ControlIcon from './ControlIcon';
 import { toggleParticipantProperty } from '../../../../actions/broadcast';
 import { connectPrivateCall, chatWithParticipant, sendToStage, kickFanFromFeed } from '../../../../actions/producer';
@@ -15,7 +15,7 @@ import './Participant.css';
 const isBackstageFan = R.equals('backstageFan');
 const isOnStageFan = R.equals('fan');
 
-const getHeaderLabel = (type: string): string => R.toUpper(getRoleTranslation(type));
+const getHeaderLabel = (type: string): string => R.toUpper(translateRole(type));
 
 type OwnProps = {
   type: ParticipantType
@@ -71,7 +71,7 @@ const Participant = (props: Props): ReactComponent => {
       </div>
       { isBackstageFan(type) ?
         <div className="Participant-move-fan">
-          <button className="move btn transparent" onClick={sendFanToStage}>Move to {getRoleTranslation('fan')} feed</button>
+          <button className="move btn transparent" onClick={sendFanToStage}>Move to {translateRole('fan')} feed</button>
         </div> :
         <div className="Participant-url">
           <span className="url">{ url }</span>
--- src/components/Broadcast/Producer/components/ProducerSidePanel.js
+++ src/components/Broadcast/Producer/components/ProducerSidePanel.js
@@ -3,14 +3,14 @@ import React from 'react';
 import classNames from 'classnames';
 import ActiveFanList from './ActiveFanList';
 import './ProducerSidePanel.css';
-import { getRoleTranslation, properCase } from '../../../../services/util';
+import { translateRole, properCase } from '../../../../services/util';
 
 type Props = { hidden: boolean, broadcast: BroadcastState };
 
 const ProducerSidePanel = ({ hidden, broadcast }: Props): ReactComponent =>
   <div className={classNames('ProducerSidePanel', { hidden })} >
     <div className="ProducerSidePanel-header">
-      Active {`${properCase(getRoleTranslation('fan'))}s`}&nbsp;
+      Active {`${properCase(translateRole('fan'))}s`}&nbsp;
       ({ broadcast.activeFans.order.length })
     </div>
     <ActiveFanList activeFans={broadcast.activeFans} />
