e4a569276fdfe3cde5b0bf157df1e080bf289f2f
deviantfero <fmorataya.04@gmail.com>
2018-11-08 16:45:36 -0600

    change text in UI using translate function

--- src/components/Broadcast/Producer/components/ProducerSidePanel.js
+++ src/components/Broadcast/Producer/components/ProducerSidePanel.js
@@ -3,12 +3,16 @@ import React from 'react';
 import classNames from 'classnames';
 import ActiveFanList from './ActiveFanList';
 import './ProducerSidePanel.css';
+import { getRoleTranslation, properCase } from '../../../../services/util';
 
 type Props = { hidden: boolean, broadcast: BroadcastState };
 
 const ProducerSidePanel = ({ hidden, broadcast }: Props): ReactComponent =>
   <div className={classNames('ProducerSidePanel', { hidden })} >
-    <div className="ProducerSidePanel-header">Active Fans ({ broadcast.activeFans.order.length }) </div>
+    <div className="ProducerSidePanel-header">
+      Active {`${properCase(getRoleTranslation('fan'))}s`}&nbsp;
+      ({ broadcast.activeFans.order.length })
+    </div>
     <ActiveFanList activeFans={broadcast.activeFans} />
   </div>;
 
