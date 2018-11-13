5275aded4067d8a89c9992ae6e7b6d47265b864f
deviantfero <fmorataya.04@gmail.com>
2018-11-13 16:22:10 -0600

    load admin colors on signup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,45 @@
 // @flow
 import React from 'react';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import Logout from '../Logout/Logout';
 import logo from '../../images/uni-brand-wide.png';
 import './Header.css';
 
-type Props = {
+type BaseProps = {
+  user: CurrentUserState
+};
+
+type InitialProps = {
   routes: Route[]
 };
 
+type Props = InitialProps & BaseProps;
+
 const DefaultLogo = (): ReactComponent => <div className="Header-logo"><img src={logo} alt="opentok" /></div>;
 
-const Header = ({ routes }: Props): ReactComponent => {
+const Header = ({ routes, user }: Props): ReactComponent => {
+  const html = document.getElementsByTagName('html')[0];
   const currentRoute = routes[routes.length - 1];
+  const siteColor = user && user.siteColor;
+
+  if (siteColor) {
+    html.style.setProperty('--main-background-bg', siteColor);
+    html.style.setProperty('--main-btn-bg', siteColor);
+    html.style.setProperty('--link-color', siteColor);
+  }
+
   if (currentRoute.hideHeader) return null;
   return (
-    <div className="Header">
+    <div className="Header" style={{ backgroundColor: siteColor || 'default' }}>
       <DefaultLogo />
       <Logout />
     </div>
   );
 };
 
+const mapStateToProps = (state: State): BaseProps => ({
+  user: state.currentUser,
+});
 
-export default withRouter(Header);
+export default withRouter(connect(mapStateToProps, null)(Header));
