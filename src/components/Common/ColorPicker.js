788d9e6281d942fc81e25460b1864a4f8520997c
deviantfero <fmorataya.04@gmail.com>
2018-11-14 14:34:07 -0600

    support for rgba, decide to show black or white foreground based on background

--- src/components/Common/ColorPicker.js
+++ src/components/Common/ColorPicker.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { Component } from 'react';
-import { ChromePicker } from 'react-color';
+import { SketchPicker } from 'react-color';
 import './ColorPicker.css';
 
 type Color = {
@@ -31,6 +31,8 @@ class ColorPicker extends Component {
 
     this.state = {
       showPicker: false,
+      color: '#ffffff',
+      border: 'none'
     };
 
     this.togglePicker = this.togglePicker.bind(this);
@@ -42,19 +44,30 @@ class ColorPicker extends Component {
   }
 
   changeHandler(c: Color, e: SyntheticInputEvent) {
-    this.props.onChange(c.hex, e);
-    this.togglePicker();
+    const rgba = `rgba(${c.rgb.r}, ${c.rgb.g}, ${c.rgb.b}, ${c.rgb.a})`
+
+    const color = c.hsl.l > 0.70 ? 'black' : 'white';
+    const border = c.hsl.l > 0.70 ? '1px solid black' : 'none';
+    const value = c.rgb.a < 1 ? rgba : c.hex;
+
+    this.setState({ color, border });
+    this.props.onChange(value, e);
   }
 
   render(): ReactComponent {
     const value = this.props.value || '#000000';
     const text = this.props.text || value;
+    const buttonStyle = {
+      backgroundColor: value,
+      border: this.state.border,
+      color: this.state.color
+    };
 
     return (
       <div className="colorpicker">
         <button
           type="button"
-          style={{ backgroundColor: value, minWidth: '90px', maxWidth: '90px' }}
+          style={buttonStyle}
           className="btn action"
           onClick={this.togglePicker}
         >
@@ -63,8 +76,7 @@ class ColorPicker extends Component {
         {
           this.state.showPicker &&
             <div className="colorpicker-container">
-              <ChromePicker
-                disableAlpha
+              <SketchPicker
                 color={value}
                 onChangeComplete={this.changeHandler}
               />
