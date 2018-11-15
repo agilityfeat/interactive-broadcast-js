01da56aa8a2ea6ea3894eff6b430c1b4e858931b
deviantfero <fmorataya.04@gmail.com>
2018-11-15 16:01:40 -0600

    remove settings listening from header, now in Routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -28,39 +28,20 @@ type LogoProps = {
 
 type Props = InitialProps & BaseProps & DispatchProps;
 
-class Header extends React.Component {
-  constructor(props: Props) {
-    super(props);
+const Header = (props: Props): ReactComponent => {
+  const { routes, settings: { siteLogo, siteFavicon } } = props;
+  const currentRoute = routes[routes.length - 1];
 
-    props.listenSiteSettings();
-  }
+  if (currentRoute.hideHeader) return null;
+  return (
+    <div className="Header">
+      <Favicon url={(siteFavicon && siteFavicon.url) || favicon} />
+      <Logo src={(siteLogo && siteLogo.url) || logo} />
+      <Logout />
+    </div>
+  );
+};
 
-  componentDidUpdate(prevProps: Props) {
-    const { siteColor } = this.props.settings;
-    const prevSiteColor = prevProps.settings.siteColor;
-
-    if (prevSiteColor !== siteColor) {
-      const html = document.getElementsByTagName('html')[0];
-      html.style.setProperty('--main-header-bg', siteColor);
-      html.style.setProperty('--main-btn-bg', siteColor);
-      html.style.setProperty('--link-color', siteColor);
-    }
-  }
-
-  render(): ReactComponent {
-    const { routes, settings: { siteLogo, siteFavicon } } = this.props;
-    const currentRoute = routes[routes.length - 1];
-
-    if (currentRoute.hideHeader) return null;
-    return (
-      <div className="Header">
-        <Favicon url={(siteFavicon && siteFavicon.url) || favicon} />
-        <Logo src={(siteLogo && siteLogo.url) || logo} />
-        <Logout />
-      </div>
-    );
-  }
-}
 const Logo = ({ src }: LogoProps): ReactComponent => <div className="Header-logo"><img src={src} alt="opentok" /></div>;
 
 const mapStateToProps = (state: State): BaseProps => ({
