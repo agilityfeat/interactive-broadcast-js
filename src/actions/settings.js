1d30594857c898249b9ba735aa48d5ec6b1a2308
deviantfero <fmorataya.04@gmail.com>
2018-11-15 13:42:48 -0600

    choose between loading the default images or user defined ones

--- src/actions/settings.js
+++ src/actions/settings.js
@@ -1,20 +1,29 @@
 // @flow
+import R from 'ramda';
 import firebase from '../services/firebase';
 
 const listenSiteSettings: ThunkActionCreator = (): Thunk =>
   (dispatch: Dispatch) => {
     try {
-      const ref = firebase.database()
-            .ref('admins')
-            .orderByChild('domain')
-            .equalTo(window.location.hostname);
+      firebase.auth().onAuthStateChanged(async (user): AsyncVoid => {
+        if (user) {
+          const ref = firebase.database()
+                .ref('admins')
+                .orderByChild('domain')
+                .equalTo(window.location.hostname);
 
-      ref.on('value', (snapshot: firebase.database.DataSnapshot) => {
-        const admin = snapshot.val();
-        const { siteColor, registerEnabled } = admin[Object.keys(admin)[0]];
-        const settings = { siteColor, registerEnabled };
+          ref.on('value', (snapshot: firebase.database.DataSnapshot) => {
+            const admin = snapshot.val();
+            const settings = R.pick(
+              ['siteColor', 'registerEnabled', 'siteLogo', 'siteFavicon'],
+              admin[Object.keys(admin)[0]],
+            );
 
-        dispatch({ type: 'SET_SITE_SETTINGS', settings });
+            dispatch({ type: 'SET_SITE_SETTINGS', settings });
+          });
+        } else {
+          await firebase.auth().signInAnonymously();
+        }
       });
     } catch (error) {
       // eslint-disable-next-line no-console
