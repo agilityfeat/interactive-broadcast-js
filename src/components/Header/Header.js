4424b4a09c9a2df49365333c325008b2541121ec
deviantfero <fmorataya.04@gmail.com>
2018-11-15 16:53:02 -0600

    remove setting favicon from header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import Favicon from 'react-favicon';
 import Logout from '../Logout/Logout';
 import logo from '../../images/uni-brand-wide.png';
-import favicon from '../../images/favicon.ico';
 import { listenSiteSettings } from '../../actions/settings';
 import './Header.css';
 
@@ -29,13 +27,12 @@ type LogoProps = {
 type Props = InitialProps & BaseProps & DispatchProps;
 
 const Header = (props: Props): ReactComponent => {
-  const { routes, settings: { siteLogo, siteFavicon } } = props;
+  const { routes, settings: { siteLogo } } = props;
   const currentRoute = routes[routes.length - 1];
 
   if (currentRoute.hideHeader) return null;
   return (
     <div className="Header">
-      <Favicon url={(siteFavicon && siteFavicon.url) || favicon} />
       <Logo src={(siteLogo && siteLogo.url) || logo} />
       <Logout />
     </div>
