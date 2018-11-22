f1c32d611fef584f4eb978defc4a52bc43bf1016
deviantfero <fmorataya.04@gmail.com>
2018-11-22 15:41:32 -0600

    add adminId prop, fetch event before showing

--- src/components/RegisterViewer/RegisterViewer.js
+++ src/components/RegisterViewer/RegisterViewer.js
@@ -2,8 +2,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import R from 'ramda';
+import { getEventByKey } from '../../services/api';
 import { setSuccess } from '../../actions/alert';
 import { userForgotPassword } from '../../actions/auth';
+import Loading from '../Common/Loading';
+import NoEvents from '../Common/NoEvents';
 import RegisterViewerForm from './components/RegisterViewerForm';
 import LoginViewerForm from './components/LoginViewerForm';
 import logo from '../../images/uni-brand-wide.png';
@@ -12,6 +15,7 @@ import '../Header/Header.css';
 
 type BaseProps = {
   userUrl: string,
+  adminId: string,
   settings: Settings,
   event: BroadcastEvent
 };
@@ -26,7 +30,9 @@ type Props = BaseProps & DispatchProps;
 const Logo = ({ src }: LogoProps): ReactComponent => <div className="Header-logo"><img src={src} alt="opentok" /></div>;
 class RegisterViewer extends React.Component {
   state: {
-    register: boolean
+    register: boolean,
+    noEvent: boolean,
+    event?: BroadcastEvent
   };
 
   toggleRegister: Unit;
@@ -36,7 +42,14 @@ class RegisterViewer extends React.Component {
 
     this.state = {
       register: false,
+      event: null,
+      noEvent: false,
     };
+
+    // use the URL adminId instead of settings.id
+    getEventByKey(props.adminId, props.userUrl)
+      .then((event?: BroadcastEvent): void => this.setState({ event }))
+      .catch((): void => this.setState({ noEvent: true }));
 
     this.toggleRegister = this.toggleRegister.bind(this);
   }
@@ -47,20 +60,27 @@ class RegisterViewer extends React.Component {
 
   render(): ReactComponent {
     const { settings, onSuccess, userUrl } = this.props;
-    const { register } = this.state;
+    const { register, noEvent, event } = this.state;
+    const startImage = event && event.startImage.url;
+    const eventName = event && event.name;
+
+    if (noEvent) return <NoEvents />;
+    if (!event) return <Loading />;
 
     return (
       <div>
         <div className="Header">
           <Logo src={(settings.siteLogo && settings.siteLogo.url) || logo} />
+          <h3>Join {eventName}</h3>
         </div>
         <div className="RegisterViewer">
           <div className="RegisterViewer-header" >
-            <img src={(settings.siteLogo && settings.siteLogo.url) || logo} alt="logo" />
+            <img src={startImage || (settings.siteLogo && settings.siteLogo.url) || logo} alt="logo" />
           </div>
           {
             register &&
             <div className="RegisterViewer-body">
+              <h4>Create account for {window.location.host}</h4>
               <RegisterViewerForm onSuccess={onSuccess} settings={settings} />
               <button onClick={this.toggleRegister} className="btn transparent">
                 I already have an account
@@ -70,6 +90,7 @@ class RegisterViewer extends React.Component {
           {
             !register &&
             <div className="RegisterViewer-body">
+              <h4>Sign in to {window.location.host}</h4>
               <LoginViewerForm userUrl={userUrl} onSuccess={onSuccess} settings={settings} />
               <button onClick={this.toggleRegister} className="btn transparent">
                 {'I don\'t have an account'}
