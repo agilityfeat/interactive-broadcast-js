0ab86c08b26233b4db38ffaedb776c89e14d1a2a
deviantfero <fmorataya.04@gmail.com>
2018-11-22 13:46:28 -0600

    change AuthRoutes for AdminRoutes

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ import Users from './components/Users/Users';
 import UpdateEvent from './components/UpdateEvent/UpdateEvent';
 import ViewEvent from './components/ViewEvent/ViewEvent';
 import Producer from './components/Broadcast/Producer/Producer';
-import AuthRoutes from './components/AuthRoutes/AuthRoutes';
+import AdminRoutes from './components/AuthRoutes/AdminRoutes';
 import CelebrityHost from './components/Broadcast/CelebrityHost/CelebrityHost';
 import Fan from './components/Broadcast/Fan/Fan';
 import { listenSiteSettings } from './actions/settings';
@@ -33,7 +33,7 @@ const routes = (
       <Route path="/show/:adminId" component={Fan} hideHeader embed userType={'fan'} />
       <Route path="/show-host/:adminId" component={CelebrityHost} hideHeader embed userType={'host'} />
       <Route path="/show-guest/:adminId" component={CelebrityHost} hideHeader embed userType={'celebrity'} />
-      <Route component={AuthRoutes}>
+      <Route component={AdminRoutes}>
         <Route path="admin" component={Dashboard} />
         <Route path="users" component={Users} />
         <Route path="users/:adminId" component={Users} />
