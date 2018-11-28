414b5d77b82f9c02144ed4431574542747c56450
deviantfero <fmorataya.04@gmail.com>
2018-11-28 17:44:24 -0600

    add an async method to load correct domain's information where appro

--- src/services/eventUrls.js
+++ src/services/eventUrls.js
@@ -1,24 +1,36 @@
+// @flow
+import Hashids from 'hashids';
 import R from 'ramda';
 import { remove as removeDiacritics } from 'diacritics';
-import Hashids from 'hashids';
+import { getDomain } from './api';
 
 // eslint-disable-next-line no-regex-spaces
 const convertName = R.compose(R.replace(/ /g, '-'), R.toLower, R.replace(/  +/g, ' '), removeDiacritics, R.replace(/[^a-zA-Z0-9 ]/g, ''), R.trim);
-const origin = (): string => window.location.origin;
 const hashEventName = (name: string): string => R.isEmpty(name) ? '' : new Hashids(name).encode(1, 2, 3);
 
 // We still need to return urls when no name is provided as it provides the base for the urls in the event form
-const createUrls = ({ name, adminId }: { name?: string, adminId: string}): EventUrls => {
-  if (!adminId) { return {}; }
-  const base = origin();
+export const createUrls = ({ name, domainId }: { name?: string, domainId: string}): EventUrls => {
+  if (!domainId) { return {}; }
   const eventName = convertName(name);
   const eventNameHash = hashEventName(eventName);
   return {
-    fanUrl: `${base}/show/${adminId}/${eventName}`,
-    fanAudioUrl: `${base}/post-production/${adminId}/${eventName}`,
-    hostUrl: `${base}/show-host/${adminId}/${eventNameHash}`,
-    celebrityUrl: `${base}/show-guest/${adminId}/${eventNameHash}`,
+    fanUrl: `${window.location.host}/show/${domainId}/${eventName}`,
+    fanAudioUrl: `${window.location.host}/post-production/${domainId}/${eventName}`,
+    hostUrl: `${window.location.host}/show-host/${domainId}/${eventNameHash}`,
+    celebrityUrl: `${window.location.host}/show-guest/${domainId}/${eventNameHash}`,
   };
 };
 
-export default createUrls;
+export const loadUrls = async ({ name, domainId }: { name?: string, domainId: string}): EventUrls => {
+  if (!domainId) { return {}; }
+  const domain = await getDomain(domainId);
+  const eventName = convertName(name);
+  const eventNameHash = hashEventName(eventName);
+  if (!domain) { return {}; }
+  return {
+    fanUrl: `${domain.domain}/show/${domainId}/${eventName}`,
+    fanAudioUrl: `${domain.domain}/post-production/${domainId}/${eventName}`,
+    hostUrl: `${domain.domain}/show-host/${domainId}/${eventNameHash}`,
+    celebrityUrl: `${domain.domain}/show-guest/${domainId}/${eventNameHash}`,
+  };
+};
