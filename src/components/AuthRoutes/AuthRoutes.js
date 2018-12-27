2d9513c5b9d8b70f379505e1536ff9657601ff7c
deviantfero <fmorataya.04@gmail.com>
2018-12-26 19:24:39 -0600

    check if register is enabled before redirecting

--- src/components/AuthRoutes/AuthRoutes.js
+++ src/components/AuthRoutes/AuthRoutes.js
@@ -6,23 +6,31 @@ import { browserHistory } from 'react-router';
 
 type Props = {
   children: ReactComponent[],
-  currentUser: User
+  currentUser: User,
+  settings: Settings
 };
 
 class AuthRoutes extends Component {
   props: Props;
 
   componentWillMount() {
-    if (!this.props.currentUser) {
+    const { currentUser, settings } = this.props;
+    const authorized = currentUser || !settings.registerEnabled;
+
+    if (!authorized) {
       browserHistory.replace('/');
     }
   }
 
   render(): ReactComponent {
-    return this.props.currentUser ? this.props.children : null;
+    const { currentUser, settings } = this.props;
+    const authorized = currentUser || !settings.registerEnabled;
+
+    return authorized ? this.props.children : null;
   }
 }
 
-const mapStateToProps = (state: State): Props => R.pick(['currentUser'], state);
+const mapStateToProps = (state: State): Props =>
+  R.pick(['currentUser', 'settings'], state);
 export default connect(mapStateToProps)(AuthRoutes);
 
