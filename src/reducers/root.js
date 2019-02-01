e0d0bcac69793f6e952f3cf8bd56fd3c6ff1d08a
deviantfero <fmorataya.04@gmail.com>
2019-02-01 13:26:00 -0600

    include file reducer in root redux reducer

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -10,6 +10,7 @@ import currentUser from './currentUser';
 import domains from './domains';
 import events from './events';
 import fan from './fan';
+import files from './files';
 import settings from './settings';
 import users from './users';
 
@@ -22,6 +23,7 @@ const interactiveBroadcastApp = combineReducers({
   domains,
   events,
   fan,
+  files,
   settings,
   toastr,
   users,
